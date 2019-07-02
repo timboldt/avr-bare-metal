cacfbf79cfa6a38fd74ec49369aaf15064a47357
Tim Boldt <tim.boldt@gmail.com>
2019-07-01 23:14:16 -0700

    Cleaned up main.c blinky

--- main.c
+++ main.c
@@ -1,54 +1,19 @@
-////////////////////////////////////////////////////
-/*
-Baremetal Programming on the Atmel
-ATMega328P (Arduino) processor.
-Dana Olcott
-12/15/17
+#include <avr/io.h>
+#include <stdbool.h>
+#include <util/delay.h>
 
-Ex: A simple program that flashes Pin 13.
+int main() {
+  const int kDelay = 500;
 
-Pin Descriptions:
-Pins 0 to 13 reside on two ports: PORTD and PORTB
+  // Note: PB5 is pin 13 on the Uno which is the built-in LED.
+  DDRB |= _BV(DDB5);
 
-Output Registers:
-Pins 0 to 7 - PORTD: PORTD0 - PORTD7
-Pins 8 to 13 - PORTB: PORTB0 - PORTB5
+  while (true) {
+    PORTB |= _BV(PORTB5);
+    _delay_ms(kDelay);
+    PORTB &= ~_BV(PORTB5);
+    _delay_ms(kDelay);
+  }
 
-Direction Register (set bit to enable output):
-Pins 0 to 7 - DDRD _BV(DDD0) - DRD _BV(DDD7)
-Pins 8 to 13 - DDRB _BV(DDB0) - DRB _BV(DDB5)
-
-Defines (see makefile)  __AVR_ATmega328P__
-Inludes:  /usr/lib/avr/include
-*/
-///////////////////////////////////////////////
-
-#include <avr/io.h>         //macros
-#include <util/delay.h>     //_delay_ms function
-
-#define DELAY_MS       100
-
-
-int main()
-{
-	//register init - DDRB - PortB, DDB5 - Pin13
-	DDRB |= _BV(DDB5);      //PB5 - pin 13 output
-	
-
-    while(1)
-    {
-
-        //Set Pins 0 to 13
-        PORTB |= _BV(PORTB5);       //pin 13
-      
-
-        _delay_ms(DELAY_MS);
-
-        //Clear Pins 0 to 13
-        PORTB &=~ _BV(PORTB5);       //pin 13
-        
-        _delay_ms(DELAY_MS);
-    }
-
-	return 0;
+  return 0;
 }
